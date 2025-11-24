cc778f15bf836eb255bac986e210fa0bb6275d8e
Christofer Bäcklin <backlin@users.noreply.github.com>
2025-11-24 12:19:27 +0100

    Add install script for Ubuntu 24.04.3 LTS

---
+++ install_ubuntu_2404.sh
@@ -0,0 +1,25 @@
+apt update
+apt upgrade -y
+
+apt install git -y
+apt install pre-commit -y
+apt install 7zip -y
+apt install curl -y
+apt install make -y
+apt install just -y
+
+apt install zsh -y
+apt install vim -y
+apt install tmux -y
+apt install ripgrep -y
+apt install zoxide -y
+apt install fzf -y
+apt install eza -y # eza.rocks
+
+apt install golang-go -y
+apt install rustup -y
+
+apt install gimp -y
+apt install sqlitebrowser -y
+apt install vlc -y
+
